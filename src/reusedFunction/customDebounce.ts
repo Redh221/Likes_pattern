3d45036a26cb42f347debcda641d7518c67e6ede
Grack <grackbox@gmail.com>
2024-10-07 15:35:46 +0500

    fixed

--- src/reusedFunction/customDebounce.ts
+++ src/reusedFunction/customDebounce.ts
@@ -1,17 +1,25 @@
 import { pubSub } from "../App";
 
-export function debouncerInit(eventName: string, fn: (...args: any[]) => void) {
+export function debouncerInit(
+  eventName: string,
+  fn: (...args: any[]) => void,
+  fnInner: (...args: any[]) => void
+) {
   let timer: ReturnType<typeof setTimeout> | undefined;
+  pubSub.unsubscribe("sendLikes", fn);
   const debounce = (...args: any[]) => {
     clearTimeout(timer);
 
     timer = setTimeout(() => {
       pubSub.unsubscribe(eventName, debounce);
-
-      fn(...args);
+      fnInner(...args);
       pubSub.subscribe(eventName, fn);
     }, 3000);
   };
+  timer = setTimeout(() => {
+    pubSub.unsubscribe(eventName, debounce);
+    pubSub.subscribe(eventName, fn);
+  }, 3000);
   pubSub.subscribe(eventName, debounce);
   console.log(pubSub.cache);
 }
--- src/reusedFunction/localStorageUtils.ts
+++ src/reusedFunction/localStorageUtils.ts
@@ -6,30 +6,35 @@ export const saveAllToLocalStorage = (
   par: CurrentUserIdT,
   booleanPar: boolean
 ) => {
-  if (localStorage.getItem("ids") === null) {
-    localStorage.setItem("ids", JSON.stringify([]));
-  }
-
-  const storedData = localStorage.getItem("ids");
-
-  if (storedData) {
-    let parsedData = JSON.parse(storedData);
-
-    if (Array.isArray(parsedData)) {
-      if (parsedData.find((item) => item == par)) {
-        if (!booleanPar) {
-          parsedData = parsedData.filter((item) => item !== par);
-        }
-        console.log(`${par} имеется`);
-      } else if (booleanPar) {
-        parsedData.push(par);
-      }
-    } else {
-      parsedData = [par];
+  const saveAllToLocalStorageInner = (
+    par: CurrentUserIdT,
+    booleanPar: boolean
+  ) => {
+    if (localStorage.getItem("ids") === null) {
+      localStorage.setItem("ids", JSON.stringify([]));
     }
 
-    localStorage.setItem("ids", JSON.stringify(parsedData));
-  }
-  pubSub.unsubscribe("sendLikes", saveAllToLocalStorage);
-  debouncerInit("sendLikes", saveAllToLocalStorage);
+    const storedData = localStorage.getItem("ids");
+
+    if (storedData) {
+      let parsedData = JSON.parse(storedData);
+
+      if (Array.isArray(parsedData)) {
+        if (parsedData.find((item) => item == par)) {
+          if (!booleanPar) {
+            parsedData = parsedData.filter((item) => item !== par);
+          }
+          console.log(`${par} имеется`);
+        } else if (booleanPar) {
+          parsedData.push(par);
+        }
+      } else {
+        parsedData = [par];
+      }
+
+      localStorage.setItem("ids", JSON.stringify(parsedData));
+    }
+  };
+  saveAllToLocalStorageInner(par, booleanPar);
+  debouncerInit("sendLikes", saveAllToLocalStorage, saveAllToLocalStorageInner);
 };
